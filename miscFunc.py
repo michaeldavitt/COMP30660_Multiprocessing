dd5ab5fff4b0115c2341887ede6b30a181e06cb6
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-26 10:27:07 +0000

    Compares runtime when using 1 and 4 cores

--- miscFunc.py
+++ miscFunc.py
@@ -1,5 +1,8 @@
 import math
 import multiprocessing
+import time
+import matplotlib.pyplot as plt
+
 
 
 def my_func(x):
@@ -11,3 +14,24 @@ def my_func_verbose(x):
     s = math.sqrt(x)
     print("Task", multiprocessing.current_process(), x, s)
     return s
+
+
+def check_prime(num):
+    t1 = time.time()
+    res = False
+    if num > 0:
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                print(i, "times", num//i, "is", num)
+                print("Time:", int(time.time()-t1))
+                break
+        else:
+            print(num, "is a prime number")
+            print("Time:", time.time()-t1)
+            res = True
+            # if input number is less than
+            # or equal to 1, it is not prime
+    print("Task", multiprocessing.current_process(), num, res)
+    return res
