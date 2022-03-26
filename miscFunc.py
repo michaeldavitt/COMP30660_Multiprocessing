df7cbe6b650c05cb37ce29694a71455250de6ec8
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-26 08:38:15 +0000

    Initial commit

---
+++ miscFunc.py
@@ -0,0 +1,13 @@
+import math
+import multiprocessing
+
+
+def my_func(x):
+    s = math.sqrt(x)
+    return s
+
+
+def my_func_verbose(x):
+    s = math.sqrt(x)
+    print("Task", multiprocessing.current_process(), x, s)
+    return s
